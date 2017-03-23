6bb3fb320710bdaf93abaa82c1e0179c0625ba84
shawn_liu <sxl4023@rit.edu>
2017-03-23 14:38:28 -0400

    finished midterm practice

--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -16,6 +16,35 @@ void AppClass::Update(void)
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
 	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+
+	float fTimer = dTotalTime;
+
+	// rotation
+	m_m4Steve = glm::rotate(m_m4Steve, -fTimer * 72, vector3(0.0f, 0.0f, 1.0f));
+
+	// translation
+	vector3 translation;
+	float fPercentage = MapValue(fTimer, 0.0f, 5.0f, 0.0f, 1.0f);
+
+	vector3 p1 = vector3(0.0f, 0.0f, 0.0f);
+	vector3 p2 = vector3(5.0f, 0.0f, 0.0f);
+
+	int direction = (int)floor(fPercentage);
+
+	switch (direction % 2) {
+	case 0:
+		translation = glm::lerp(p1, p2, fPercentage - floor(fPercentage));
+		break;
+	case 1:
+		translation = glm::lerp(p2, p1, fPercentage - floor(fPercentage));
+		break;
+	}
+	// apply translation
+	m_m4Steve = m_m4Steve * glm::translate(translation);
+
+
+
+
 #pragma endregion
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
